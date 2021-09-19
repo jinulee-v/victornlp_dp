cb9799f4b6a371d63adc74d1e5915efbb9eecf8a
Jinu Lee <aquamrn@snu.ac.kr>
2021-09-19 20:14:12 +0900

    change parse.py for spreadsheet usage(from master) and modified distance calculation for KMDP

--- victornlp_dp/tools/analyze/accuracy.py
+++ victornlp_dp/tools/analyze/accuracy.py
@@ -5,6 +5,8 @@ Tools for analyzong the parser behavior such as accuracy().
 
 from . import register_analysis_fn
 
+from ...kmdp.lexicals import _all_heads
+
 @register_analysis_fn('accuracy')
 def analyze_accuracy(inputs):
   """
@@ -21,8 +23,6 @@ def analyze_accuracy(inputs):
     assert 'dependency' in input
     assert 'dependency_predict' in input
 
-    if len(input['dependency']) != len(input['dependency_predict']):
-      print(len(input['dependency']), len(input['dependency_predict']))
     for golden, predict in zip(input['dependency'], input['dependency_predict']):
       if predict['dep'] != golden['dep']:
         continue
@@ -82,7 +82,15 @@ def analyze_accuracy_per_distance(inputs):
       if predict['dep'] != golden['dep']:
         continue
       
-      key = 'ROOT' if golden['head'] == 0 else abs(golden['head'] - golden['dep'])
+      if golden['head'] == 0:
+        key = 'ROOT'
+      else:
+        key = abs(golden['head'] - golden['dep'])
+        for wp in input['pos']:
+          for morph in wp:
+            if min(golden['head'], golden['dep']) < morph['id'] < max(golden['head'], golden['dep']):
+              if morph['pos_tag'] not in _all_heads:
+                key -= 1
 
       if key not in total:
         total[key] = 0
@@ -92,3 +100,49 @@ def analyze_accuracy_per_distance(inputs):
         unlabel[key] += 1
 
   return {key:unlabel[key]/total[key] for key in total.keys()}
+
+@register_analysis_fn('accuracy_semi_las')
+def analyze_accuracy_same_head_pos(inputs):
+  """
+  Calculates semi-LAS accuracy.
+  semi-LAS: accuracy for 1. correct label 2. correct head part-of-speech(not the correct head but same label)
+  
+  @param inputs List of dictionaries. Refer to 'dataset.py' for more details.
+  
+  @return Dictionary containing semi-LAS for each label.
+  """
+  total = {}
+  semi_labeled = {}
+  for input in inputs:
+    assert 'dependency' in input
+    assert 'dependency_predict' in input
+
+    for golden, predict in zip(input['dependency'], input['dependency_predict']):
+      if predict['dep'] != golden['dep']:
+        continue
+      
+      key = golden['label']
+
+      if predict['label'] == golden['label']:
+        # find head label
+        golden_pos = predict_pos = None
+        for arc in input['dependency']:
+          if arc['dep'] == golden['head']:
+            golden_pos = arc['label'].split('_')[0]
+          if arc['dep'] == predict['head']:
+            predict_pos = arc['label'].split('_')[0]
+
+        if key not in total:
+          total[key] = 0
+          semi_labeled[key] = 0
+        total[key] += 1
+        if golden_pos is not None and predict_pos is not None:
+          # not ROOT dependency
+          if golden_pos == predict_pos:
+            semi_labeled[key] += 1
+        else:
+          # ROOT dependency
+          if predict['head'] == 0:
+            semi_labeled[key] += 1
+
+  return {key:semi_labeled[key]/total[key] for key in total.keys()}
